30bab52d12759a4caf7cb8f0aee8359168fcca73
Ilya <i.smirnov10@uniyar.ac.ru>
2024-06-05 18:45:51 +0300

    Create rocket.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <SFML/Graphics.hpp>
+#include <ctime>
 
 using namespace sf;
 
+class Rocket {
+public:
+    std::pair<unsigned, unsigned> size{ 12,23 };
+    std::pair<unsigned, unsigned> place;
+    Clock clock;
+
+    Rocket() :place((rand() * 1100) / RAND_MAX, (rand() * 500) / RAND_MAX) {}
+
+    bool Booom()
+    {
+        if (this->size.first >= 100 && this->size.second >= 100)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void fall()
+    {
+        size.first *= 1.2;
+        size.second *= 1.2;
+    }
+};
+
 int main()
 {
+    srand(time(0));
     RenderWindow window(sf::VideoMode(1200, 600), "fallout");
     Image background_image;
     background_image.loadFromFile("images/nebo.jpg");
@@ -11,10 +40,20 @@ int main()
     t_nebo.loadFromImage(background_image);
     Sprite nebo(t_nebo);
 
+    Rocket r;
+
     while (window.isOpen())
     {
         window.clear();
         window.draw(nebo);
+
+        Texture t_rocket;
+        t_rocket.loadFromFile("images/missile00.png");
+        Sprite rocket(t_rocket);
+        rocket.setTextureRect(IntRect(10, 3, 12, 23));
+        rocket.setScale(r.size.first / 12.0, r.size.second / 23.0);
+        rocket.setPosition(r.place.first, r.place.second);
+        window.draw(rocket);
 
         sf::Event event;
         while (window.pollEvent(event))
