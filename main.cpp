c0537b975befbacf4ba82b92f02e690153b94baa
Ilya <i.smirnov10@uniyar.ac.ru>
2024-06-05 18:52:28 +0300

    Update rocket.cpp
    сделал случайную генерацию ракет и их движение

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <ctime>
+#include <vector>
 
 using namespace sf;
+using namespace std;
 
 class Rocket {
 public:
@@ -35,31 +37,46 @@ int main()
     srand(time(0));
     RenderWindow window(sf::VideoMode(1200, 600), "fallout");
     Image background_image;
-    background_image.loadFromFile("images/nebo.jpg");
+    background_image.loadFromFile("Images/nebo.jpg");
     Texture t_nebo;
     t_nebo.loadFromImage(background_image);
     Sprite nebo(t_nebo);
 
-    Rocket r;
+    vector<Rocket> rockets(1);
+
+    Clock global_time;
 
     while (window.isOpen())
     {
         window.clear();
         window.draw(nebo);
 
-        Texture t_rocket;
-        t_rocket.loadFromFile("images/missile00.png");
-        Sprite rocket(t_rocket);
-        rocket.setTextureRect(IntRect(10, 3, 12, 23));
-        rocket.setScale(r.size.first / 12.0, r.size.second / 23.0);
-        rocket.setPosition(r.place.first, r.place.second);
-        window.draw(rocket);
-
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+        }
+
+
+        Texture t_rocket;
+        t_rocket.loadFromFile("Images/missile00.png");
+        Sprite rocket(t_rocket);
+        for (int i = 0; i < rockets.size(); ++i)
+        {
+            rocket.setScale(rockets[i].size.first / 12.0, rockets[i].size.second / 23.0);
+            rocket.setPosition(rockets[i].place.first, rockets[i].place.second);
+            window.draw(rocket);
+            if (!rockets[i].Booom() && rockets[i].clock.getElapsedTime() >= milliseconds(300))
+            {
+                rockets[i].fall();
+                rockets[i].clock.restart();
+            }
+        }
+        if (global_time.getElapsedTime() >= milliseconds(1100))
+        {
+            rockets.push_back(Rocket());
+            global_time.restart();
         }
         window.display();
     }
